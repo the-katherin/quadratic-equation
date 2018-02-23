f45085d32d0e6a0a05bcfc7fb02a474a9f179327
the-katherin <ekaterina082612@gmail.com>
2018-02-23 16:34:25 +0300

    solution not 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr = equation.split(' ');
+  var a = arr[0];
+  var c = arr[8];
+  var b = arr[4];
+  var x1;
+  var x2;
+  var result;
+  var signOfB = arr[3];
+  var signOfC = arr[7];
+  if (signOfB == '-'){
+    b = -b;
+    }
+  if (signOfC == '-'){
+    c = -c;
+    }
+  var discr = b*b-4*a*c;
+
+  if (discr>0) {
+    x1 = ((-b) + Math.sqrt(discr)) / (2*a);
+    x2 = ((-b) - Math.sqrt(discr)) / (2*a);
+    result = [x1, x2];
+    }
+  else if (discr === 0) {
+    x1= (-b)/(2*a);
+    x2=x1;
+    result = [x1, x2];
+    }
+  else result = 0;
+  var sortResult = result.sort();
+  return sortResult;
+
+
 }
